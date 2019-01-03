5e8dbd62481617808d297264799830a42b36023a
Dusto´n Lomrt <simon.m.f.w.knott@eclipso.de>
2019-01-03 22:24:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,40 +1,33 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  position = gets.strip
+  index = input_to_index(position)
+  move(board, index)
+end
 
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]}  "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]}  "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
+def input_to_index(input)
+  index = input.to_i - 1
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8)
-    if !position_taken?(board, index)
-      true
-    end
-  end
+  space_empty = board[index] == " " || board[index] == "" || board[index] == nil
+  index_valid = index.between?(0,8)
+  space_empty && index_valid
 end
 
-def turn(user_input)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if valid_move?(board, index) == true
-    move(board, index)
-    display_board(board)
+def move(board, index, value = "X")
+  if valid_move?(board, index)
+    board[index] = value
+	  display_board(board)
   else
     turn(board)
   end
-end
-
-def position_taken?(board, index)
-  board[index] != " "
-end
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
 end
